aad587a187ccce1f22f87c9c624bff36f18bc853
nasihulumam17 <nasihulumam15@gmail.com>
2021-09-29 13:57:20 +0700

    first commit

---
+++ tugas1.js
@@ -0,0 +1,12 @@
+// Buat array baru
+var recipe = ["Step1", "Step2", "Step3"];
+
+//  Akses Step Pertama
+console.log(recipe[0]);
+
+
+// Buat Variabel last step
+var last_step = recipe[2];
+
+// Akses step terakhir dari variabel last_step
+console.log(last_step);
---
+++ tugas2.js
@@ -0,0 +1,19 @@
+// membuat array baru dengan variabel my_bucket_list
+my_bucket_list = ["Lulus Kuliah", "Bekerja", "Menikah", "Bahagia"];
+console.log(my_bucket_list)
+
+// Menambahkan array di index paling belakang dengan push
+my_bucket_list.push("Owner Bisnis");
+console.log(my_bucket_list);
+
+// menambahkan array di index paling depan dengan unshift
+my_bucket_list.unshift("Mandiri Finansisal");
+console.log(my_bucket_list);
+
+// Menghapus item terakhir dengan pop
+my_bucket_list.pop(0);
+console.log(my_bucket_list);
+
+// Menghapus item pertama dengan shift
+my_bucket_list.shift();
+console.log(my_bucket_list);
